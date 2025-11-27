6ec1362995de42fd2e667ac5cdb1c2d9f552870f
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-11-27 12:12:33 +0545

    test

--- envs/staging/main.tf
+++ envs/staging/main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 module "vpc" {
   source               = "../../modules/vpc"
   environment          = var.environment
+  vpc_cidr             = "10.0.0.0/16"
   public_subnet1_cidr  = "10.0.1.0/24"
   public_subnet2_cidr  = "10.0.2.0/24"
   private_subnet1_cidr = "10.0.3.0/24"
