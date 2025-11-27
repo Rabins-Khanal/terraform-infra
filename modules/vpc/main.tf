de5d6d6bc6e7700c65c40059aa2fa13ebaeaabc8
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-11-27 11:10:00 +0545

    test

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -76,7 +76,7 @@ resource "aws_route_table" "public" {
 
 # Associate public route table
 resource "aws_route_table_association" "public" {
-  subnet_id      = aws_subnet.public.id
+  subnet_id      = aws_subnet.public.ids
   route_table_id = aws_route_table.public.id
 }
 
