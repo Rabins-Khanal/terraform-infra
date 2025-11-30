c41e53901e16787ee87b77ebd3972dd4596a3ba2
Rabins Khanal <rk@MacBook-Air-3.local>
2025-11-30 20:47:01 +0545

    blue green test

--- modules/ec2/userdata.sh
+++ modules/ec2/userdata.sh
@@ -1,5 +1,2 @@
-sudo apt update -y
-sudo apt install apache2 mysql-server php php-mysql libapache2-mod-php -y
-sudo systemctl enable apache2
-sudo systemctl start apache2
-
+#!/bin/bash
+echo "Hello world" > /tmp/test.txt
