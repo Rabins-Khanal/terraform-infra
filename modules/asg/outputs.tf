b8610ae6a834938366a4de4da896f8f6f08454e0
Rabins Khanal <rk@MacBook-Air-3.local>
2025-11-30 22:14:44 +0545

    blue green test

--- modules/asg/outputs.tf
+++ modules/asg/outputs.tf
@@ -1,3 +1,5 @@
 output "asg_blue_id" { value = aws_autoscaling_group.blue.id }
-output "asg_green_id" { value = aws_autoscaling_group.green.id }
+output "asg_green_id" {
+  value = var.deploy_green ? aws_autoscaling_group.green[0].id : ""
+}
 output "asg_sg_id" { value = aws_security_group.asg_sg.id }
