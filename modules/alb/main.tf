f30d8275731745e59b319b6e8a65323d094aa94e
Rabins Khanal <rk@MacBook-Air-3.local>
2025-11-30 19:48:47 +0545

    blue green test

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -1,8 +1,38 @@
+##########################
+# ALB Security Group
+##########################
+resource "aws_security_group" "alb_sg" {
+  name        = "alb-sg-${var.environment}"
+  description = "Allow HTTP access to ALB"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(var.tags, {
+    Name = "alb-sg-${var.environment}"
+  })
+}
+
+##########################
+# Application Load Balancer
+##########################
 resource "aws_lb" "this" {
   name               = "alb-${var.environment}"
   load_balancer_type = "application"
   internal           = false
-  security_groups    = [var.alb_sg_id]
+  security_groups    = [aws_security_group.alb_sg.id]
   subnets            = var.public_subnet_ids
 
   tags = merge(var.tags, {
@@ -10,6 +40,9 @@ resource "aws_lb" "this" {
   })
 }
 
+##########################
+# Target Groups
+##########################
 resource "aws_lb_target_group" "blue" {
   name        = "tg-blue-${var.environment}"
   port        = 80
@@ -50,13 +83,16 @@ resource "aws_lb_target_group" "green" {
   })
 }
 
+##########################
+# Listener
+##########################
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.this.arn
   port              = 80
   protocol          = "HTTP"
 
   default_action {
-    type             = "forward"
+    type = "forward"
     forward {
       target_group {
         arn    = aws_lb_target_group.blue.arn
--- modules/alb/outputs.tf
+++ modules/alb/outputs.tf
@@ -3,3 +3,4 @@ output "listener_arn" { value = aws_lb_listener.http.arn }
 output "tg_blue_arn" { value = aws_lb_target_group.blue.arn }
 output "tg_green_arn" { value = aws_lb_target_group.green.arn }
 output "dns_name" { value = aws_lb.this.dns_name }
+output "alb_sg_id" { value = aws_security_group.alb_sg.id }
