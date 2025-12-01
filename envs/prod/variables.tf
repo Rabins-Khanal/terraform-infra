27a17f62fa35eeec7ee6851db4137d25dbe8d286
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-12-01 12:02:36 +0545

    test

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,5 +1,5 @@
 variable "environment" { default = "prod" }
-variable "ami_id" { default = "ami-02b8269d5e85954ef" }
+variable "ami_id" { default = "ami-09a7289256f9ab2c7" }
 
 variable "db_username" { default = "admin" }
 variable "db_password" { default = "Password123!" }
