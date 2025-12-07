575c91d50541950fa53694b6f002495dd184d3b2
Rabins Khanal <rk@MacBook-Air-3.local>
2025-12-07 12:32:53 +0545

    blue green test

--- envs/prod/main.tf
+++ envs/prod/main.tf
@@ -23,23 +23,6 @@ module "vpc" {
   tags                 = local.tags
 }
 
-module "codedeploy" {
-  source                 = "../../modules/codedeploy"
-  environment            = var.environment
-  tags                   = local.tags
-  artifact_bucket_name   = "wordpress-artifact-${var.environment}"
-  codedeploy_app_name    = "wordpress-app-${var.environment}"
-  deployment_group_name  = "wordpress-prod-dg"
-  asg_blue_name          = module.asg.asg_blue_name
-  asg_green_name         = module.asg.asg_green_name
-  tg_blue_arn            = module.alb.tg_blue_arn
-  tg_green_arn           = module.alb.tg_green_arn
-  tg_blue_name           = module.alb.tg_blue_name
-  tg_green_name          = module.alb.tg_green_name
-  listener_arn           = module.alb.listener_arn
-  terminate_wait_minutes = 5
-}
-
 module "alb" {
   source            = "../../modules/alb"
   environment       = var.environment
@@ -62,4 +45,21 @@ module "asg" {
   ec2_instance_profile = module.codedeploy.ec2_instance_profile
   user_data_file       = "../../modules/ec2/userdata.sh"
   tags                 = local.tags
+}
+
+module "codedeploy" {
+  source                 = "../../modules/codedeploy"
+  environment            = var.environment
+  tags                   = local.tags
+  artifact_bucket_name   = "wordpress-artifact-${var.environment}"
+  codedeploy_app_name    = "wordpress-app-${var.environment}"
+  deployment_group_name  = "wordpress-prod-dg"
+  asg_blue_name          = module.asg.asg_blue_name
+  asg_green_name         = module.asg.asg_green_name
+  tg_blue_arn            = module.alb.tg_blue_arn
+  tg_green_arn           = module.alb.tg_green_arn
+  tg_blue_name           = module.alb.tg_blue_name
+  tg_green_name          = module.alb.tg_green_name
+  listener_arn           = module.alb.listener_arn
+  terminate_wait_minutes = 5
 }
