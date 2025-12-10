815876db99f4963839622db606f051fdf0b8d2ed
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-12-10 13:20:42 +0545

    igw added

--- envs/prod/variables.tf
+++ envs/prod/variables.tf
@@ -1,5 +1,5 @@
 variable "environment" { default = "prod" }
-variable "ami_id" { default = "ami-09a7289256f9ab2c7" }
+variable "ami_id" { default = "ami-087ab4242a51f7ae4" }
 
 variable "db_username" { default = "admin" }
 variable "db_password" { default = "Password123!" }
