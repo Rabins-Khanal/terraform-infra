7ec618fa585b4dba45580b937af5298be617a867
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-12-15 09:57:55 +0545

    removing code deploy group

--- modules/codedeploy/outputs.tf
+++ modules/codedeploy/outputs.tf
@@ -1,4 +1,3 @@
 output "artifact_bucket" { value = aws_s3_bucket.artifacts.id }
 output "codedeploy_app_name" { value = aws_codedeploy_app.app.name }
-output "codedeploy_deployment_group" { value = aws_codedeploy_deployment_group.dg.deployment_group_name }
 output "ec2_instance_profile" { value = aws_iam_instance_profile.ec2_profile.name }
--- modules/codedeploy/variables.tf
+++ modules/codedeploy/variables.tf
@@ -2,7 +2,6 @@ variable "environment" { type = string }
 variable "tags" { type = map(string) }
 variable "artifact_bucket_name" { type = string }
 variable "codedeploy_app_name" { type = string }
-variable "deployment_group_name" { type = string }
 variable "asg_blue_name" { type = string }
 variable "listener_arn" { type = string } # ALB listener ARN
 variable "terminate_wait_minutes" {
