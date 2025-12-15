7473a809a64eb25b637aec03e3593b4015c29f3b
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-12-15 10:17:09 +0545

    removing code deploy group

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -51,11 +51,12 @@ resource "aws_lb" "this" {
 # Target Groups
 #########################
 resource "aws_lb_target_group" "blue" {
-  name        = "tg-blue-${var.environment}"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
+  name                 = "tg-blue-${var.environment}"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "instance"
+  deregistration_delay = 900
 
   health_check {
     path                = "/"
@@ -71,11 +72,12 @@ resource "aws_lb_target_group" "blue" {
 }
 
 resource "aws_lb_target_group" "green" {
-  name        = "tg-green-${var.environment}"
-  port        = 80
-  protocol    = "HTTP"
-  vpc_id      = var.vpc_id
-  target_type = "instance"
+  name                 = "tg-green-${var.environment}"
+  port                 = 80
+  protocol             = "HTTP"
+  vpc_id               = var.vpc_id
+  target_type          = "instance"
+  deregistration_delay = 900
 
   health_check {
     path                = "/"
