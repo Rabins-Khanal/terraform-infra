631f719a5ed480874e3685fab50f3a75a2c07312
Rabins-Khanal <rabinskhanal47@gmail.com>
2025-12-23 10:44:00 +0545

    test

--- modules/asg/variables.tf
+++ modules/asg/variables.tf
@@ -5,10 +5,6 @@ variable "ami_id" { type = string }
 variable "alb_sg_id" { type = string }
 variable "tg_blue_arn" { type = string }
 variable "tags" { type = map(string) }
-variable "user_data_file" {
-  description = "Path to userdata.sh for EC2 instances"
-  type        = string
-}
 
 variable "ec2_instance_profile" {
   type = string
